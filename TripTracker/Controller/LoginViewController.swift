94a3f2aa5990ee2fd3aad16a430b8da13b0e88b6
S. M. Hasibur Rahman <hasib.jhd@gmail.com>
2021-06-23 13:46:43 +0600

    [Trip Tracker] Project directory set up is done [Details] Entry point programmatically changed

--- TripTracker/Controller/LoginViewController.swift
+++ TripTracker/Controller/LoginViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  LoginViewController.swift
 //  TripTracker
 //
 //  Created by S. M. Hasibur Rahman on 6/23/21.
@@ -8,13 +8,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoginViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        view.backgroundColor = .red
     }
-
-
 }
-
