2646c5a5b37f135d1fcc6da5e4725d9f31f6c1af
S. M. Hasibur Rahman <hasib.jhd@gmail.com>
2021-06-23 22:00:31 +0600

    [TRIPTracker] Login page is designed, some asset files have been added

--- TripTracker/Controller/LoginViewController.swift
+++ TripTracker/Controller/LoginViewController.swift
@@ -12,7 +12,7 @@ class LoginViewController: UIViewController {
 
     // MARK: - Properties
     
-    private var titleLabel: UILabel = {
+    private let titleLabel: UILabel = {
         let label = UILabel()
         label.text = "TRIP TRACKER"
         label.font = UIFont(name: "Avenir-Light", size: 36)
@@ -20,14 +20,58 @@ class LoginViewController: UIViewController {
         return label
     }()
     
+    private let emailTextField = UITextField().getFieldWithExtraFeature(textLabel: "Email", isSecured: false)
+    private let passwordTextField = UITextField().getFieldWithExtraFeature(textLabel: "Password", isSecured: true)
+    private lazy var emailContainerView = UIView().getViewWithContainerView(image: #imageLiteral(resourceName: "ic_mail_outline_white_2x"),textField: emailTextField)
+    private lazy var passwordContainerView = UIView().getViewWithContainerView(image: #imageLiteral(resourceName: "ic_lock_outline_white_2x"),textField: passwordTextField)
+    
+    private let loginButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Login", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.backgroundColor = .mainBlueTint
+        button.layer.cornerRadius = 5
+        return button
+    }()
+    
+    private let signupButton: UIButton = {
+        let button = UIButton(type: .system)
+        let titleFirstPart = NSMutableAttributedString(string: "Don't have an Account?", attributes:
+            [NSAttributedString.Key.font : UIFont.systemFont(ofSize: 16),
+            NSMutableAttributedString.Key.foregroundColor: UIColor.lightGray])
+        let titleSecondPart = NSMutableAttributedString(string: " SignUp", attributes:
+        [NSAttributedString.Key.font : UIFont.systemFont(ofSize: 16),
+            NSMutableAttributedString.Key.foregroundColor: UIColor.mainBlueTint])
+        
+        titleFirstPart.append(titleSecondPart)
+        button.setAttributedTitle(titleFirstPart, for: .normal)
+        return button
+    }()
+    
     // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        view.backgroundColor = UIColor.init(red: 25/255, green: 25/255, blue: 25/255, alpha: 1)
+        view.backgroundColor = .backgroundColor
         
         view.addSubview(titleLabel)
         titleLabel.anchor(topAnchor: view.safeAreaLayoutGuide.topAnchor, centerXParentView: view)
+        
+        
+        let stackView = UIStackView(arrangedSubviews: [emailContainerView,passwordContainerView,loginButton])
+        stackView.axis = .vertical
+        stackView.distribution = .fillEqually
+        stackView.spacing = 16
+        
+        view.addSubview(stackView)
+        stackView.anchor(topAnchor: titleLabel.bottomAnchor, topPadding: 50,
+                         leftAnchor: view.leftAnchor, leftPadding: 30,
+                         rightAnchor: view.rightAnchor, rightPadding: 30)
+        
+        view.addSubview(signupButton)
+        signupButton.anchor(bottomAnchor: view.safeAreaLayoutGuide.bottomAnchor, bottomPadding: 20,
+                            leftAnchor: view.leftAnchor, leftPadding: 30,
+                            rightAnchor: view.rightAnchor, rightPadding: 30)
     }
     
     override var preferredStatusBarStyle: UIStatusBarStyle {
--- TripTracker/Utils/Extensions.swift
+++ TripTracker/Utils/Extensions.swift
@@ -9,13 +9,37 @@
 import UIKit
 
 extension UIView {
+    
+    func getViewWithContainerView(image: UIImage, textField: UITextField) -> UIView {
+        let view = UIView()
+        view.backgroundColor = .black
+        let imageView = UIImageView()
+        imageView.image = image
+        
+        
+        view.addSubview(imageView)
+        imageView.anchor(leftAnchor: view.leftAnchor,
+                         heigtConstant: 24, widthConstant: 24,
+                         centerYParentView: view)
+        view.addSubview(textField)
+        textField.anchor(leftAnchor: imageView.rightAnchor, leftPadding: 10,
+                              rightAnchor: view.rightAnchor,  centerYParentView: view)
+        let separatorView = UIView()
+        view.addSubview(separatorView)
+        separatorView.backgroundColor = .white
+        separatorView.anchor(bottomAnchor: view.bottomAnchor, leftAnchor: view.leftAnchor,
+                             rightAnchor: view.rightAnchor, heigtConstant: 0.75)
+        
+        return view
+    }
+    
     func anchor(topAnchor: NSLayoutYAxisAnchor? = nil,
-                bottomAnchor: NSLayoutYAxisAnchor? = nil,
-                leftAnchor: NSLayoutXAxisAnchor? = nil,
-                rightAnchor: NSLayoutXAxisAnchor? = nil,
                 topPadding: CGFloat = 0,
+                bottomAnchor: NSLayoutYAxisAnchor? = nil,
                 bottomPadding: CGFloat = 0,
+                leftAnchor: NSLayoutXAxisAnchor? = nil,
                 leftPadding: CGFloat = 0,
+                rightAnchor: NSLayoutXAxisAnchor? = nil,
                 rightPadding: CGFloat = 0,
                 heigtConstant: CGFloat? = nil,
                 widthConstant: CGFloat? = nil,
@@ -29,7 +53,7 @@ extension UIView {
         }
         
         if let bottomAnchor = bottomAnchor {
-            self.bottomAnchor.constraint(equalTo: bottomAnchor, constant: bottomPadding).isActive = true
+            self.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -bottomPadding).isActive = true
         }
         
         if let leftAnchor = leftAnchor {
@@ -37,7 +61,7 @@ extension UIView {
         }
         
         if let rightAnchor = rightAnchor {
-            self.rightAnchor.constraint(equalTo: rightAnchor, constant: rightPadding).isActive = true
+            self.rightAnchor.constraint(equalTo: rightAnchor, constant: -rightPadding).isActive = true
         }
         
         if let height = heigtConstant {
@@ -57,4 +81,26 @@ extension UIView {
         }
         
     }
+}
+
+
+extension UITextField {
+    func getFieldWithExtraFeature(textLabel: String, isSecured: Bool) -> UITextField{
+        textColor = .white
+        font = UIFont.systemFont(ofSize: 16)
+        keyboardAppearance = .dark
+        isSecureTextEntry = isSecured
+        attributedPlaceholder = NSAttributedString(string: textLabel,
+                                                   attributes: [NSAttributedString.Key.foregroundColor : UIColor.lightGray])
+        return self
+    }
+}
+
+extension UIColor {
+    static func rgb(red: CGFloat, green: CGFloat, blue: CGFloat) -> UIColor {
+        return UIColor.init(red: red/255, green: green/255, blue: blue/255, alpha: 1.0)
+    }
+    
+    static let backgroundColor = UIColor.rgb(red: 25, green: 25, blue: 25)
+    static let mainBlueTint = UIColor.rgb(red: 17, green: 154, blue: 237)
 }
